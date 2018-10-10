c9a1deb6447f309cd49768d11fb0e3cd54237822
grapefruit623 <grapefruit623@gmail.com>
2018-10-10 18:48:46 +0800

    leetcode 48_rotateImage
    Just practice to use unittest module.

--- medium/48_rotateImage.py
+++ medium/48_rotateImage.py
@@ -27,19 +27,35 @@ class Solution:
 
 class testRotateMatrix(unittest.TestCase):
 
+    def setUp(self):
+        self.sol = Solution()
+    
     def test_case1(self):
-        sol = Solution()
         matrix = [ [1,2,3], [4,5,6], [7,8,9] ]
         result = [ [7,4,1], [8,5,2], [9,6,3] ]
-        sol.rotate(matrix)
-        print (result == matrix)
+        self.sol.rotate(matrix)
+        self.assertEqual(matrix, result)
 
     def test_case2(self):
-        sol = Solution()
         matrix = [ [5,1,9,11], [2,4,8,10], [13,3,6,7], [15,14,12,16] ]
         result = [ [15,13,2,5], [14,3,4,1], [12,6,8,9], [16,7,10,11] ]
-        sol.rotate(matrix)
-        print (result == matrix)
+        self.sol.rotate(matrix)
+        self.assertEqual(matrix, result)
+
+    def test_case3(self):
+        matrix = [ [1,1], [2,2] ]
+        result = [ [1,1], [2,2] ]
+        self.sol.rotate(matrix)
+        self.assertNotEqual(matrix, result)
 
 if __name__ == "__main__":
-    unittest.main()
+    trueSuite = unittest.TestSuite()
+    trueSuite.addTest(testRotateMatrix('test_case1'))
+    trueSuite.addTest(testRotateMatrix('test_case2'))
+
+    falseSuite = unittest.TestSuite()
+    falseSuite.addTest(testRotateMatrix('test_case3'))
+
+    runner = unittest.TextTestRunner()
+    runner.run(trueSuite)
+    runner.run(falseSuite)
