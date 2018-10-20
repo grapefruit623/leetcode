9f6d0aff6c77658418da37ce2db9c99663c1a01b
grapefruit623 <grapefruit623@gmail.com>
2018-10-20 14:52:52 +0800

    208 implementTrie
    AC
    
    Implement a trie
    
    Reference:
    http://pisces.ck.tp.edu.tw/~peng/index.php?action=showfile&file=f743c2923f8170798f62a585257fdd8436cd73b6d
    http://www.cnblogs.com/huangxincheng/archive/2012/11/25/2788268.html

--- medium/208_implementTrie.py
+++ medium/208_implementTrie.py
@@ -4,16 +4,33 @@
 import unittest
 
 """
-    Use python's building method
-    TLE
+    2018/10/19:
+        Use python's building method
+        TLE
+        Remember this question's title: trie!
+        trie is tree data structure.
+
+    2018/10/20:
+        AC
+        Implement a simple trie 
 """
 class Trie:
     
+    class TrieNode():
+        def __init__(self, word):
+            self.char = word
+            self.childs = {}
+            self.count = 0
+
+        def __repr__(self):
+            return str(self.char) +"- "+ str(self.childs)
+
+
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.strList = []
+        self.root = self.TrieNode(None)
 
     def insert(self, word):
         """
@@ -21,18 +38,32 @@ class Trie:
         :type word: str
         "rtype: void
         """
-        self.strList.append(word)
+        node = self.root
+        for c in word:
+            if c not in node.childs:
+                node.childs[c] = self.TrieNode(c)
 
-    def search(self, word):
+            node = node.childs[c]
+
+        node.count = 1
+
+    def search(self, word, isStartWith=False):
         """
         Returns if the word is in the trie.
         :type word: str
         :rtype: bool
         """
-        if word in self.strList:
-            return True
-        else:
+        node = self.root
+        for c in word:
+            if c not in node.childs:
+                return False
+            else:
+                node = node.childs[c]
+
+        if not isStartWith and node.count == 0:
             return False
+
+        return True
 
     def startsWith(self, prefix):
         """
@@ -40,11 +71,7 @@ class Trie:
         :type prefix: str
         :rtype: bool
         """
-        for s in self.strList:
-            if s.startswith(prefix):
-                return True
-
-        return False
+        return self.search(prefix, isStartWith=True)
 
 class Unittest_implementTrie(unittest.TestCase):
     def setUp(self):
@@ -52,17 +79,35 @@ class Unittest_implementTrie(unittest.TestCase):
 
     def test_insert(self):
         self.trie.insert("apple")
-        self.assertEqual( ["apple"], self.trie.strList )
+        self.assertEqual(None, self.trie.root.char)
+        self.assertEqual("a", self.trie.root.childs['a'].char)
+        self.assertEqual("p", self.trie.root.childs['a'].childs['p'].char)
+        self.assertEqual("p", self.trie.root.childs['a'].childs['p'].childs['p'].char)
+        self.assertEqual("l", self.trie.root.childs['a'].childs['p'].childs['p'].childs['l'].char)
+        self.assertEqual("e", self.trie.root.childs['a'].childs['p'].childs['p'].childs['l'].childs['e'].char)
 
     def test_search(self):
         self.trie.insert("apple")
         result = self.trie.search("apple")
         self.assertEqual(True, result)
 
+    def test_search_fail(self):
+        self.trie.insert("apple")
+        result = self.trie.search("app")
+        self.assertEqual(False, result)
+
     def test_startsWith(self):
         self.trie.insert("apple")
         result = self.trie.startsWith("app")
         self.assertEqual(True, result)
+
+    def test_wa_case1(self):
+        self.trie.insert("apple")
+        self.assertEqual(True, self.trie.search("apple"))
+        self.assertEqual(False, self.trie.search("app"))
+        self.assertEqual(True, self.trie.startsWith("app"))
+        self.trie.insert("app")
+        self.assertEqual(True, self.trie.search("app"))
 
     def tearDown(self):
         self.trie = None
