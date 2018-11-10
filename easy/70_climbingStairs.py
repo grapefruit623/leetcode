be86f3ee98d61c5f4efe1e8d2d797d925f55e7c8
grapefruit623 <grapefruit623@gmail.com>
2018-11-10 13:29:06 +0800

    Renew leetcode 70 climbing stairs.
    Using fibonacci formula to create more faster solution.

--- easy/70_climbingStairs.py
+++ easy/70_climbingStairs.py
@@ -9,6 +9,18 @@ class Solution(object):
     def __init__(self):
         self.fibonacci = [1,1]
 
+    """
+        fibonacci formula solution
+    """
+    def climbStairsV2(self, n):
+        import math
+        fn = ( 1/math.sqrt(5) )*( math.pow( ( (1+math.sqrt(5))/2 ), n+1 ) - 
+                                    math.pow( ( (1-math.sqrt(5))/2 ), n+1) )
+        return int(fn)
+
+    """
+        Dynamic programing solution
+    """
     def climbStairs(self, n):
         """
         :type n: int
@@ -44,6 +56,16 @@ class Unittest_climbStairs(unittest.TestCase):
         data = 3
         expected = 3
         self.assertEqual(expected, self.sol.climbStairs(data))
+
+    def test_example1_v2(self):
+        data = 2
+        expected = 2
+        self.assertEqual(expected, self.sol.climbStairsV2(data))
+
+    def test_example2_v2(self):
+        data = 3
+        expected = 3
+        self.assertEqual(expected, self.sol.climbStairsV2(data))
 
     def tearDown(self):
         self.sol = None
