2360fc9a5bf07e4645cc0f04bd02e66b65fac1cf
grapefruit623 <grapefruit623@gmail.com>
2019-08-03 15:03:23 +0800

    leetcode 486 predict the winner
    AC
    
    Further to think about how to refact to dynamic programing.

--- medium/486_predictTheWinner.py
+++ medium/486_predictTheWinner.py
@@ -4,52 +4,59 @@ from typing import List
 
 class Solution:
     '''
-        TLE
-        reference: Solution comes from Approach #1 Using Recursion [Accepted]
+        AC
+        Using recursion and memorization.
+        Concept of solution comes from Approach #1 Using Recursion [Accepted]
+
     '''
     def PredictTheWinner(self, nums:List[int])->bool:
-        print (nums)
-        p1Score = self.calculateScore(nums, 0, len(nums)-1, 1)
+        dp = {}
+        p1Score = self.calculateScore(nums, 0, len(nums)-1, 1, dp)
         print ('p1Score - p2Score: ', p1Score)
         return True if p1Score >= 0 else False
 
 
-    def calculateScore(self, nums, i, j, turn):
+    def calculateScore(self, nums, i, j, turn, dp):
+        if (i,j) in dp.keys():
+            return dp[(i,j)]
+
         if i == j:
-            return turn * nums[i]
+            dp[(i,j)] = turn * nums[i]
         else:
-            l = self.calculateScore(nums, i+1, j, -1*turn)
-            l = turn * nums[i] + l 
-            r = self.calculateScore(nums, i, j-1, -1*turn)
-            r = turn * nums[j] + r 
+            l = turn * nums[i] + self.calculateScore(nums, i+1, j, -1*turn, dp)
+            r = turn * nums[j] + self.calculateScore(nums, i, j-1, -1*turn, dp) 
 
             if turn == 1:
-                return max(l, r)
+                dp[(i,j)] = max(l,r)
             else:
-                return min(l,r)
+                dp[(i,j)] = min(l,r)
 
+        return dp[(i,j)]
 
 class Unittest_predictTheWinnder(unittest.TestCase):
     def setUp(self):
         self.sol = Solution()
 
-    '''
     def test_sample1(self):
         data = [1,5,2]
         expected = False
         self.assertEqual(expected, self.sol.PredictTheWinner(data))
-    '''
 
-    '''
     def test_sample2(self):
         data = [1,5,233,7]
         expected = True
         self.assertEqual(expected, self.sol.PredictTheWinner(data))
-    '''
+
     def test_sample3(self):
         data = [1,5,7]
         expected = True
         self.assertEqual(expected, self.sol.PredictTheWinner(data))
+
+    def test_sample4(self):
+        data = [504,427,95,397,468,485,326,112,296,290,106,148,12,334,23,296,122,187,141,187]
+        expected = True
+        self.assertEqual(expected, self.sol.PredictTheWinner(data))
+
 
 if __name__ == '__main__':
     unittest.main()
