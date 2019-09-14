86cfeb4487f56d6b142e3a18b86002a1ddc1f0e4
grapefruit623 <grapefruit623@gmail.com>
2019-09-14 14:17:06 +0800

    leetcode 877 stone games
    Try to enhance performance by prune, but still slowly.

--- medium/877_stoneGame.py
+++ medium/877_stoneGame.py
@@ -9,34 +9,31 @@ class Solution:
     def stoneGame(self, piles: List[int])->bool:
         self.piles = piles
         self.dp = {}
-        scoreDiff = self.miniMax(0, len(self.piles)-1, 1)
+        self.miniMax(0, len(self.piles)-1, 1)
 
-        print ('scoreDiff: ', scoreDiff)
-        if scoreDiff > 0:
-            return True
-        else:
-            return False
+        return True if self.dp[(0, len(self.piles)-1, 1)] > 0 else False
 
     def miniMax(self, i, j, isAlexTurn=1):
+        if (i,j,isAlexTurn) in self.dp.keys():
+            return
         if i == j:
-            return self.piles[i]*isAlexTurn
+            self.dp[ (i,j,isAlexTurn) ] = self.piles[i]*isAlexTurn
         else:
             leftDp = 0
             rightDp = 0
-            if (i+1,j,-1*isAlexTurn) not in self.dp.keys(): 
-                self.dp[ (i+1, j, -1*isAlexTurn) ] = self.miniMax(i+1,j, -1*isAlexTurn)
 
+            if (i+1,j,-1*isAlexTurn) not in self.dp.keys(): 
+                self.miniMax(i+1,j, -1*isAlexTurn)
             left = isAlexTurn * self.piles[i] + self.dp[(i+1, j, -1*isAlexTurn)] 
 
             if (i, j-1, -1*isAlexTurn) not in self.dp.keys():
-                self.dp[(i, j-1, -1*isAlexTurn)] = self.miniMax(i, j-1, -1*isAlexTurn)
-
+                self.miniMax(i, j-1, -1*isAlexTurn)
             right = isAlexTurn * self.piles[j] + self.dp[(i, j-1, -1*isAlexTurn)] 
 
             if isAlexTurn == 1:
-                return max(left, right)
+                self.dp[ (i,j,isAlexTurn) ] = max(left, right)
             else:
-                return min(left,right)
+                self.dp[ (i,j,isAlexTurn) ] = min(left,right)
 
 class Unittest_maxProfit(unittest.TestCase):
     def setUp(self):
