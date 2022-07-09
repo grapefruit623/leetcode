86bb4fcb05775257e5279560b40e2c7d49c8af83
grapefruit623 <grapefruit623@gmail.com>
2022-07-09 17:12:04 +0800

    leetcode 15 3sum renew
    Two pointer version.

--- medium/15_3Sums.py
+++ medium/15_3Sums.py
@@ -4,6 +4,53 @@ from typing import List
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) < 3:
+            return []
+        
+        l = len(nums)
+        ans = []
+
+        nums.sort()
+                
+        start = 0
+        end = l-1
+        i = start+1
+        
+        while start < l-2:
+            target = -1*(nums[start])
+            i = start+1
+            end = l-1
+            '''
+                Two pointers
+            '''
+            while i < end:
+                temp = nums[i] + nums[end]
+                if temp == target:
+                    ans.append([nums[start], nums[i], nums[end]])
+                    
+                    # Avoid duplicated ans
+                    curr=nums[i]
+                    while i < end and nums[i] == curr:
+                        i += 1
+                    curr=nums[end]
+                    while i < end and nums[end] == curr:
+                        end -= 1
+                elif temp < target:
+                    curr=nums[i]
+                    while i < end and nums[i] == curr:
+                        i += 1
+                elif temp > target:
+                    curr=nums[end]
+                    while i < end and nums[end] == curr:
+                        end -= 1
+                        
+            curr = nums[start]
+            while start < i and nums[start] == curr:
+                start += 1
+
+        return ans
+
+    def threeSum_old(self, nums: List[int]) -> List[List[int]]:
         nums = sorted(nums)
         print (nums)
         if nums == [] or nums[0] > 0 or nums[-1] <0:
@@ -76,7 +123,7 @@ class Unittest(unittest.TestCase):
 
     def test_sample1(self):
         data = [-1,-1,2,-1,-4]
-        expected = [[-1,0,1], [-1,-1,2]]
+        expected = [[-1,-1,2]]
         self.assertEqual(expected, self.sol.threeSum(data))
 
 if __name__ == "__main__":
